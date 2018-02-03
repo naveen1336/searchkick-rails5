b01e53e802fa41d3764c5c4e0d76cf9c8fe6fac1
kav <nk1383886@gmail.com>
2018-02-03 20:39:30 +0530

    searh kick

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,20 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202050154) do
+ActiveRecord::Schema.define(version: 20150501104039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
 
-  create_table "crews", id: :serial, force: :cascade do |t|
-    t.string "name"
+  create_table "crews", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "genres", id: :serial, force: :cascade do |t|
-    t.string "name"
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -31,45 +32,35 @@ ActiveRecord::Schema.define(version: 20180202050154) do
   create_table "genres_movies", id: false, force: :cascade do |t|
     t.integer "genre_id"
     t.integer "movie_id"
-    t.index ["genre_id"], name: "index_genres_movies_on_genre_id"
-    t.index ["movie_id"], name: "index_genres_movies_on_movie_id"
   end
 
-  create_table "movies", id: :serial, force: :cascade do |t|
-    t.string "name"
-    t.text "synopsis"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "country"
-    t.integer "year"
-    t.float "review"
-    t.string "rating"
-    t.integer "runtime"
-    t.string "language"
-    t.date "release_date"
+  add_index "genres_movies", ["genre_id"], name: "index_genres_movies_on_genre_id", using: :btree
+  add_index "genres_movies", ["movie_id"], name: "index_genres_movies_on_movie_id", using: :btree
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "name"
+    t.text     "synopsis"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "country"
+    t.integer  "year"
+    t.float    "review"
+    t.string   "rating"
+    t.integer  "runtime"
+    t.string   "language"
+    t.date     "release_date"
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.float "price"
-    t.string "description"
-    t.boolean "in_stock"
+  create_table "roles", force: :cascade do |t|
+    t.integer  "movie_id"
+    t.integer  "crew_id"
+    t.string   "job"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "product_type"
-    t.string "gender"
-    t.string "brand"
   end
 
-  create_table "roles", id: :serial, force: :cascade do |t|
-    t.integer "movie_id"
-    t.integer "crew_id"
-    t.string "job"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["crew_id"], name: "index_roles_on_crew_id"
-    t.index ["movie_id"], name: "index_roles_on_movie_id"
-  end
+  add_index "roles", ["crew_id"], name: "index_roles_on_crew_id", using: :btree
+  add_index "roles", ["movie_id"], name: "index_roles_on_movie_id", using: :btree
 
   add_foreign_key "roles", "crews"
   add_foreign_key "roles", "movies"
